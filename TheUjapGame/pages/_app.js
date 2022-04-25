3439a3624349b5f9bcf69411248bbccd0ceaa33a
Temis Barreto <temispita9000@gmail.com>
2022-04-25 14:40:39 -0400

    Moralis

--- TheUjapGame/pages/_app.js
+++ TheUjapGame/pages/_app.js
@@ -2,25 +2,22 @@
 import { Layout } from "../components";
 // Styles
 import "../styles/globals.scss";
-// import { MoralisProvider } from "react-moralis";
+// Moralis Dependencies
+import { MoralisProvider } from "react-moralis";
 
 export default function MyApp({ Component, pageProps }) {
   return (
 
-
-    // <MoralisProvider
-    //   appId="gg6ViGobuRiEcaIQCAwvq7Mc2OmC4087hHilpx9e"
-    //   serverUrl="https://yufng00t8jg7.usemoralis.com:2053/server"
-    // >
-
-
     // This layout component receives <Component as a children>
       <Layout>
-        <Component {...pageProps} />
+        <MoralisProvider
+        serverUrl=""
+        appId=""
+        >
+          <Component {...pageProps} />
+        </MoralisProvider>
       </Layout>
 
-
-    // </MoralisProvider>
   );
 }
 
